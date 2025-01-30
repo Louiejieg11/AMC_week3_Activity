c5b5eb7d89a4f1693d973140a8b38b61d72ed615
Louiejieg11 <114630463+Louiejieg11@users.noreply.github.com>
2025-01-30 13:00:19 +0800

    MultiTextInput

---
+++ Activity#2/App.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import {StyleSheet, TextInput} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+const TextInputExample = () => {
+  const [name, onChangeName] = React.useState('');
+  const [age, onChangeAge] = React.useState('');
+  const [address, onChangeAddress] = React.useState('');
+  const [school, onChangeSchool] = React.useState('');
+  const [course, onChangeCourse] = React.useState('');
+  const [email, onChangeEmail] = React.useState('');
+  const [contact, onChangeContact] = React.useState('');
+  const [value, onChangeText] = React.useState('');
+
+
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeName}
+          placeholder="NAME:"
+          value={name}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeAge}
+          value={age}
+          placeholder="AGE:"
+          keyboardType="numeric"
+        />
+          <TextInput
+          style={styles.input}
+          onChangeText={onChangeAddress}
+          value={address}
+          placeholder="Address:"
+          
+        />
+            <TextInput
+          style={styles.input}
+          onChangeText={onChangeSchool}
+          value={school}
+          placeholder="SCHOOL:"
+        />
+            <TextInput
+          style={styles.input}
+          onChangeText={onChangeCourse}
+          value={course}
+          placeholder="COURSE:"
+          
+        />
+            <TextInput
+          style={styles.input}
+          onChangeText={onChangeEmail}
+          value={email}
+          placeholder="EMAIL:"
+          
+        />
+            <TextInput
+          style={styles.input}
+          onChangeText={onChangeContact}
+          value={contact}
+          placeholder="Contact no:"
+          keyboardType="numeric"
+        />
+
+         <TextInput
+          editable
+          multiline
+          numberOfLines={4}
+          maxLength={140}
+          onChangeText={text => onChangeText(text)}
+          value={value}
+          style={styles.textInput}
+          placeholder="About Me:"
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+   textInput: {
+    margin: 12,
+    padding: 10,
+    borderWidth: 1,
+  },
+});
+
+export default TextInputExample;
